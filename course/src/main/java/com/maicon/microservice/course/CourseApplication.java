56e9d4d3d8ea64e3380975bde97b7fc326f0b9fb
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2021-08-18 22:21:49 -0300

    Ajuste scan módulos

--- course/src/main/java/com/maicon/microservice/course/CourseApplication.java
+++ course/src/main/java/com/maicon/microservice/course/CourseApplication.java
@@ -2,8 +2,12 @@ package com.maicon.microservice.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.maicon.microservice.core.model")
+@EnableJpaRepositories("com.maicon.microservice.core.repository")
 public class CourseApplication {
 
 	public static void main(String[] args) {
